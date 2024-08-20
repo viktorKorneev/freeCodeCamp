aba738a6c6aa1d7f1f7d1264a5ad1402300619d8
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-20 22:36:51 +0300

    step-39

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -20,11 +20,13 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
 function goStore() {
-  console.log("Going to store.");
+  button1.innerText = "Buy 10 health (10 gold)"
+  button2.innerText = "Buy weapon (30 gold)"
+  button3.innerText = "Go to town square"
 }
 
 function goCave() {
-    button1.innerText = "Buy 10 health (10 gold)"
+    
 }
 
 function fightDragon() {

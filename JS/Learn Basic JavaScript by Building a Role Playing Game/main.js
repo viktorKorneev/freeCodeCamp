2a23c9261e8882c41b64958cedbf2b2fe3c6037c
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-20 22:19:22 +0300

    step-31

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -19,3 +19,7 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth")
+
+function goStore() {
+
+}

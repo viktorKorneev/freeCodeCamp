bbd9920ce24bfd43eb82650ccd6f2658bffb1779
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-21 18:44:46 +0300

    step-95

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -106,17 +106,20 @@ function buyHealth() {
   }
 }
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeaponIndex++;
-    goldText.innerText = gold;
-    let newWeapon = weapons[currentWeaponIndex].name;
-    text.innerText = "You now have a " + newWeapon + ".";
-    text.innerText += " In your inventory you have: " + inventory;
-    inventory.push(newWeapon)
-  } else {
-    text.innerText = "You do not have enough gold to buy a weapon."
+  if (currentWeaponIndex < 3) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+      inventory.push(newWeapon)
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon."
+    }
   }
+  
 }
 
 function fightSlime() {}

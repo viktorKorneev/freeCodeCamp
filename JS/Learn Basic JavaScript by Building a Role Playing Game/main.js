1faf0add3be68e34c26428feb3763ad04c84bf27
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-21 19:13:32 +0300

    step-110

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -38,6 +38,24 @@ const weapons = [
   },
 ];
 
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15,
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60,
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300,
+  },
+];
+
 const locations = [
   {
     name: "town square",
@@ -91,10 +109,6 @@ function goCave() {
   update(locations[2]);
 }
 
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
@@ -133,9 +147,12 @@ function sellWeapon() {
     text.innerText = "You sold a " + currentWeapon + ".";
     text.innerText += " In your inventory you have: " + inventory;
   } else {
-    text.innerText = "Don't sell your only weapon!"
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
 function fightSlime() {}
 function fightBeast() {}
+function fightDragon() {
+  console.log("Fighting dragon.");
+}

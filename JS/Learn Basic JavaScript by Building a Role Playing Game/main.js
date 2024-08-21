05a258e14def9de54d5e156cd5f008d3fe73bd90
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-21 19:06:14 +0300

    step-107

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -130,6 +130,7 @@ function sellWeapon() {
     gold += 15;
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + "." 
   }
 }
 

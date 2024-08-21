4501d6bb142829668dee3f79d165cd6df5174d28
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-21 18:08:43 +0300

    step-86

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -109,6 +109,8 @@ function buyWeapon() {
   if (gold >= 30) {
     gold -= 30;
     currentWeaponIndex++;
+    goldText.innerText = gold;
+    text.innerText = "You now have a new weapon."
   }
 }
 

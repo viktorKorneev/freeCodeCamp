fb416c05642ee526689ad2ae73d84991ba7c1d65
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-21 19:22:14 +0300

    step-113

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -151,13 +151,18 @@ function sellWeapon() {
   }
 }
 
-function fightSlime() {}
-function fightBeast() {}
-function fightDragon() {
-  console.log("Fighting dragon.");
-}
-
-function goFight() {
+function fightSlime() {
   fighting = 0;
   goFight();
 }
+
+function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
+
+function goFight() {}

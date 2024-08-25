7ba9be319b5c77667f18e1ad49bf36a8c87132f6
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-25 19:12:50 +0300

    step-118

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -174,6 +174,7 @@ function fightDragon() {
 function goFight() {
   update(locations[3])
   monsterHealth = monsters[fighting].health
+  monsterStats.style.display = "block"
 }
 
 function attack() {}

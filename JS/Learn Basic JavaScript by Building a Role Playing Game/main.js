5b59110c0ad46494b3d8ee593d21ba1586e1c108
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-25 19:39:21 +0300

    step-126

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -187,5 +187,8 @@ function attack() {
   monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
+  if (health <= 0){
+    lose()
+  }
 }
 function dodge() {}

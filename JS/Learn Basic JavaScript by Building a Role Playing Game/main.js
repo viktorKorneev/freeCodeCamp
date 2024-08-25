ca6ce095a83dbb2f39e331bb8c615eb9fd03e8be
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-25 20:10:15 +0300

    step-140

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -209,6 +209,11 @@ function attack() {
     lose();
   } else if (monsterHealth <= 0) {
     defeatMonster();
+    if (fighting === 2) {
+      winGame()
+    } else {
+      defeatMonster()
+    }
   }
 }
 function dodge() {

0ab486a909df368afcbb4c87203e9dcf9b4c3644
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-25 19:29:44 +0300

    step-121

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -181,5 +181,6 @@ function goFight() {
 
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks."
+  text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + "."
 }
 function dodge() {}

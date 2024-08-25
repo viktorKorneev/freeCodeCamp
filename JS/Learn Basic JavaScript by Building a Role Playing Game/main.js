e18bfeca19d45973a0801e4ef28d7555704936cd
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-25 19:48:16 +0300

    step-132

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -200,6 +200,8 @@ function dodge() {
 
 function defeatMonster() {
   gold += Math.floor(monsters[fighting].level * 6.7);
-  xp += monsters[fighting].level
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
 }
 function lose() {}

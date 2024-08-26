db0e6468a7ff4f7d8de19609d16a4559418a19c1
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-26 14:21:55 +0300

    step-168

--- JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
+++ JS/Learn Basic JavaScript by Building a Role Playing Game/main.js
@@ -290,7 +290,7 @@ function pick (guess){
   }
   text.innerText = "You picked " + guess() + ". Here are the random numbers:\n"
   for(let i = 0; i < 10; i++){
-    
+    text.innerText += numbers[i] + "\n"
   }
 }
 

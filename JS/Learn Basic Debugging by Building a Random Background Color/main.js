5d5fd4f387abcc5ad8f6f7a2a1f56bf81ca63e91
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-27 15:54:38 +0300

    ste-3

--- JS/Learn Basic Debugging by Building a Random Background Color/main.js
+++ JS/Learn Basic Debugging by Building a Random Background Color/main.js
@@ -1,17 +1,19 @@
 const darkColorsArr = [
-    "#2C3E50",
-    "#34495E",
-    "#2C2C2C",
-    "#616A6B",
-    "#4A235A",
-    "#2F4F4F",
-    "#0E4B5A",
-    "#36454F",
-    "#2C3E50",
-    "#800020",
-  ];
+  "#2C3E50",
+  "#34495E",
+  "#2C2C2C",
+  "#616A6B",
+  "#4A235A",
+  "#2F4F4F",
+  "#0E4B5A",
+  "#36454F",
+  "#2C3E50",
+  "#800020",
+];
 
-  function getRandomIndex() {
-    console.log(darkColorsArr.length * Math.random())
-  }
-  getRandomIndex();
+function getRandomIndex() {
+  console.log(Math.floor(darkColorsArr.length * Math.random()));
+  const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+  return randomIndex;
+}
+getRandomIndex();

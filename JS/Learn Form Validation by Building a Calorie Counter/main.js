d898dc429c0f8edb546277307e182bf329ef0217
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-28 13:54:02 +0300

    step-25

--- JS/Learn Form Validation by Building a Calorie Counter/main.js
+++ JS/Learn Form Validation by Building a Calorie Counter/main.js
@@ -7,6 +7,8 @@ const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
+    console.log("original string: ", str);
+    
   const regex = /[+-\s]/g;
     return str.replace(regex, "");
 }

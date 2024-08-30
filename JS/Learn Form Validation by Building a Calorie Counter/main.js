fc807f4e7aed0652e92a8df03b35a37d847e7bdd
viktorKorneev <korneevviktor90@yandex.ru>
2024-08-30 19:37:22 +0300

    step-61

--- JS/Learn Form Validation by Building a Calorie Counter/main.js
+++ JS/Learn Form Validation by Building a Calorie Counter/main.js
@@ -30,6 +30,7 @@ function getCaloriesFromInputs (list){
   let calories = 0
   for(const item of list){
     const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal)
   }
 }
 

c361de61fdc3ec265d28e06fb84b117e3bdb763e
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-03 19:43:38 +0300

    step-85

--- JS/Learn Form Validation by Building a Calorie Counter/main.js
+++ JS/Learn Form Validation by Building a Calorie Counter/main.js
@@ -59,7 +59,9 @@ function calculateCalories(e) {
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories
   const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit"
-  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span><hr>`;
+  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>`;
 }
 
 function getCaloriesFromInputs(list) {

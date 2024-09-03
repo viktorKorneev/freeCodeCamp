cc5658aba3bf72515e6044be9ab3c7344976ce83
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-03 20:13:21 +0300

    step-92

--- JS/Learn Form Validation by Building a Calorie Counter/main.js
+++ JS/Learn Form Validation by Building a Calorie Counter/main.js
@@ -52,19 +52,23 @@ function calculateCalories(e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
-  const budgetCalories = getCaloriesFromInputs([budgetNumberInput])
-  if(isError){
-    return 
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+  if (isError) {
+    return;
   }
-  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories
-  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories
-  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit"
-  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  const consumedCalories =
+    breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories =
+    budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(
+    remainingCalories
+  )} Calorie ${surplusOrDeficit}</span>
   <hr>
   <p>${budgetCalories} Calories Budgeted</p>
   <p>${consumedCalories} Calories Consumed</p>
   <p>${exerciseCalories} Calories Burned</p>`;
-  output.classList.remove("hide")
+  output.classList.remove("hide");
 }
 
 function getCaloriesFromInputs(list) {
@@ -84,9 +88,14 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
-function clearForm (){
-  const inputContainers = Array.from(document.querySelectorAll('.input-container')) 
+function clearForm() {
+  const inputContainers = Array.from(
+    document.querySelectorAll(".input-container")
+  );
+  for(let container of inputContainers){
+    container.innerHTML = ""
+  }
 }
 
 addEntryButton.addEventListener("click", addEntry);
-calorieCounter.addEventListener("submit", calculateCalories)
+calorieCounter.addEventListener("submit", calculateCalories);

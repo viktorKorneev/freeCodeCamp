93a4b08b6e107a934ac3f51ce7bd1bedc79e8d21
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-07 21:50:37 +0300

    step-3

--- JS/Review DOM Manipulation by Building a Rock, Paper, Scissors Game/main.js
+++ JS/Review DOM Manipulation by Building a Rock, Paper, Scissors Game/main.js
@@ -6,18 +6,36 @@ function getRandomComputerResult() {
 }
 console.log(getRandomComputerResult());
 
+let playerScore = 0;
+let computerScore = 0;
 
 function hasPlayerWonTheRound(player, computer) {
   if (player === "Rock" && computer === "Scissors") {
-    return true
+    return true;
   } else if (player === "Scissors" && computer === "Paper") {
-    return true
+    return true;
   } else if (player === "Paper" && computer === "Rock") {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
-console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
 console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (userOption === computerResult) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult}`;
+  }
+}
+
+console.log(getRoundResults("Paper"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);

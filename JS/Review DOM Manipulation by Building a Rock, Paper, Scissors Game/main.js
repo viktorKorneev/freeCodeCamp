a21e3a0ab8d628d49e73e30b53c196812e06490c
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-07 21:33:12 +0300

    step-2

--- JS/Review DOM Manipulation by Building a Rock, Paper, Scissors Game/main.js
+++ JS/Review DOM Manipulation by Building a Rock, Paper, Scissors Game/main.js
@@ -5,3 +5,19 @@ function getRandomComputerResult() {
   return options[value];
 }
 console.log(getRandomComputerResult());
+
+
+function hasPlayerWonTheRound(player, computer) {
+  if (player === "Rock" && computer === "Scissors") {
+    return true
+  } else if (player === "Scissors" && computer === "Paper") {
+    return true
+  } else if (player === "Paper" && computer === "Rock") {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));

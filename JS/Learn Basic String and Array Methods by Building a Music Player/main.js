1c71bae6917dcf731875594cdd89cbbe490f3933
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-09 15:59:26 +0300

    step-20

--- JS/Learn Basic String and Array Methods by Building a Music Player/main.js
+++ JS/Learn Basic String and Array Methods by Building a Music Player/main.js
@@ -88,5 +88,7 @@ let userData = {
 };
 
 const renderSongs = (array) => {
-  const songsHTML = array.map((song) => {});
+  const songsHTML = array.map((song) => {
+    return `<li id="song-${song.id}" class="playlist-song"></li>`;
+  });
 };

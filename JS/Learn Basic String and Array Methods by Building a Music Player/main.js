5a3e05bdb2d44ae2ffca89f81ae3427831428d66
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-09 15:48:15 +0300

    step-16

--- JS/Learn Basic String and Array Methods by Building a Music Player/main.js
+++ JS/Learn Basic String and Array Methods by Building a Music Player/main.js
@@ -87,17 +87,3 @@ let userData = {
   songCurrentTime: 0,
 };
 
-const printGreeting = () => {
-  console.log("Hello there!");
-};
-printGreeting();
-
-const printMessage = (org) => {
-  console.log(`${org} is awesome!`);
-};
-printMessage("freeCodeCamp")
-
-const addTwoNumbers = (num1, num2) => 
-    num1 + num2
-
-console.log(addTwoNumbers(3, 4));

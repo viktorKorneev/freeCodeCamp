de6bd14f4a76f081d6dc884bda1ca51dff796023
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-13 20:12:35 +0300

    step-51

--- JS/Learn Basic String and Array Methods by Building a Music Player/main.js
+++ JS/Learn Basic String and Array Methods by Building a Music Player/main.js
@@ -124,7 +124,8 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
-const getCurrentSongIndex = () => {};
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
+
 
 playButton.addEventListener("click", () => {
   if (!userData?.currentSong) {

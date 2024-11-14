f61ce5cedb71c71b14a56a27597c365f2d0976c3
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-14 15:34:09 +0300

    step-27

--- JS/Learn the Date Object by Building a Date Formatter/main.js
+++ JS/Learn the Date Object by Building a Date Formatter/main.js
@@ -13,11 +13,16 @@ currentDateParagraph.textContent = formattedDate;
 
 dateOptionsSelectElement.addEventListener("change", () => {});
 
-switch(dateOptionsSelectElement.value){
-    case "yyyy-mm-dd":
-    currentDateParagraph.textContent = formattedDate.split('-').reverse().join('-');  
-    break;  
-    case "mm-dd-yyyy-h-mm":
+switch (dateOptionsSelectElement.value) {
+  case "yyyy-mm-dd":
+    currentDateParagraph.textContent = formattedDate
+      .split("-")
+      .reverse()
+      .join("-");
+    break;
+  case "mm-dd-yyyy-h-mm":
     currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
     break;
-};
+  default:
+    currentDateParagraph.textContent = formattedDate;
+}

3d16fc7e90be747b7420f45daa6dd9ba49b9cd4d
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-14 15:20:12 +0300

    step-22

--- JS/Learn the Date Object by Building a Date Formatter/main.js
+++ JS/Learn the Date Object by Building a Date Formatter/main.js
@@ -11,10 +11,6 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-const exampleSentence = "selur pmaCedoCeerf".split('').reverse().join('');
-console.log(exampleSentence);
-
-
 dateOptionsSelectElement.addEventListener("change", () => {});
 
 switch(dateOptionsSelectElement.value){

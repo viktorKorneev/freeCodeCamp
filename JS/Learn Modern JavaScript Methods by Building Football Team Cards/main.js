dab3b636f695f214961e0f363e68a991ae06bb48
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-15 14:09:47 +0300

    step-27

--- JS/Learn Modern JavaScript Methods by Building Football Team Cards/main.js
+++ JS/Learn Modern JavaScript Methods by Building Football Team Cards/main.js
@@ -185,7 +185,7 @@ const setPlayerCards = (arr = players) => {
   playerCards.innerHTML += arr.map(
     ({ name, position, number, isCaptain, nickname }) => {
       return `<div class="player-card">
-                    <h2>${name}</h2>
+                    <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
                 </div>`;
     }
   );

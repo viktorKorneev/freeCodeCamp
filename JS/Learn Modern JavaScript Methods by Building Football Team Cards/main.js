192830dc14e2e20d864f9117421594394e71eda2
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-15 13:42:41 +0300

    step-18

--- JS/Learn Modern JavaScript Methods by Building Football Team Cards/main.js
+++ JS/Learn Modern JavaScript Methods by Building Football Team Cards/main.js
@@ -176,3 +176,4 @@ Object.freeze(myFavoriteFootballTeam);
 
 const {sport, team, year, players} = myFavoriteFootballTeam;
 const {coachName} = myFavoriteFootballTeam.headCoach;
+typeOfSport.textContent = sport;

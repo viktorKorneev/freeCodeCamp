3539982082732b6649c11a2ea6fcbadce6f5d9ff
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-16 17:15:00 +0300

    step-34

--- JS/Learn Modern JavaScript Methods by Building Football Team Cards/main.js
+++ JS/Learn Modern JavaScript Methods by Building Football Team Cards/main.js
@@ -182,14 +182,16 @@ worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) => {
-  playerCards.innerHTML += arr.map(
-    ({ name, position, number, isCaptain, nickname }) => {
+  playerCards.innerHTML += arr
+    .map(({ name, position, number, isCaptain, nickname }) => {
       return `<div class="player-card">
                     <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
                     <p>Position: ${position}</p>
                     <p>Number: ${number}</p>
                     <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
                 </div>`;
-    }
-  ).join('');
+    })
+    .join("");
 };
+
+playersDropdownList.addEventListener("change", () => {});

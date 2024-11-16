042195206a01672419f927ca980f3867adef895f
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-16 17:28:48 +0300

    step-39

--- JS/Learn Modern JavaScript Methods by Building Football Team Cards/main.js
+++ JS/Learn Modern JavaScript Methods by Building Football Team Cards/main.js
@@ -199,5 +199,6 @@ playersDropdownList.addEventListener("change", (e) => {
 
   switch (e.target.value) {
     case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
   }
 });

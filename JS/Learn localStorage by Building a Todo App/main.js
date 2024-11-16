6e3f0ef46aaba96504f6a505f2c7e3ff77965e9a
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-16 18:07:08 +0300

    step-10

--- JS/Learn localStorage by Building a Todo App/main.js
+++ JS/Learn localStorage by Building a Todo App/main.js
@@ -22,10 +22,14 @@ closeTaskFormBtn.addEventListener("click", () => {
 });
 
 cancelBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-  });
+  confirmCloseDialog.close();
+});
 
-  discardBtn.addEventListener("click", () => {
-    confirmCloseDialog.close();
-    taskForm.classList.toggle("hidden");
-  })
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});

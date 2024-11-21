7f2fcec1fa34647bc22bb20af6b01e6e51bed06b
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-21 13:06:22 +0300

    step-16

--- JS/Learn localStorage by Building a Todo App/main.js
+++ JS/Learn localStorage by Building a Todo App/main.js
@@ -34,7 +34,7 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: titleInput.value.toLowerCase().split(' '),
+    id: titleInput.value.toLowerCase().split(' ').join('-'),
   };
   console.log(taskObj);
   

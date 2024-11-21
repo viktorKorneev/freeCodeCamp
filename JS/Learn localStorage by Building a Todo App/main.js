72f06e2fcfbf5cb9024908c5a290b93c2473be4a
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-21 13:39:21 +0300

    step-28

--- JS/Learn localStorage by Building a Todo App/main.js
+++ JS/Learn localStorage by Building a Todo App/main.js
@@ -46,10 +46,13 @@ taskForm.addEventListener("submit", (e) => {
 
   taskData.forEach(({ id, title, date, description }) => {
     tasksContainer.innerHTML += `
-    <div class="task" id='${id}'></div>
-    <p><strong>Title:</strong> ${title}</p>
-    <p><strong>Date:</strong> ${date}</p>
-    <p><strong>Description:</strong> ${description}</p>
+    <div class="task" id='${id}'>
+      <p><strong>Title:</strong> ${title}</p>
+      <p><strong>Date:</strong> ${date}</p>
+      <p><strong>Description:</strong> ${description}</p>
+      <button type="button" class="btn">Edit</button>
+      <button type="button" class="btn">Delete</button>
+    </div>
     `;
   });
 });

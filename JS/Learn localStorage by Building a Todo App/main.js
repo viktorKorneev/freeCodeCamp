9406ad370a762c2142ea2808bcf983bbf4685450
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-22 18:37:34 +0300

    step-68

--- JS/Learn localStorage by Building a Todo App/main.js
+++ JS/Learn localStorage by Building a Todo App/main.js
@@ -14,20 +14,23 @@ const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
 const removeSpecialChars = (val) => {
-  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, '')
-}
+  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, "");
+};
 
 const addOrUpdateTask = () => {
-  if(!titleInput.value.trim()){
+  if (!titleInput.value.trim()) {
     alert("Please provide a title");
     return;
   }
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: titleInput.value,
+    id: `${removeSpecialChars(titleInput.value)
+      .toLowerCase()
+      .split(" ")
+      .join("-")}-${Date.now()}`,
+    title: removeSpecialChars(titleInput.value),
     date: dateInput.value,
-    description: descriptionInput.value,
+    description: removeSpecialChars(descriptionInput.value),
   };
 
   if (dataArrIndex === -1) {
@@ -124,5 +127,3 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addOrUpdateTask();
 });
-
-

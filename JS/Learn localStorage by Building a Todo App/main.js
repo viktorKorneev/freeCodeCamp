9b8310f9a2811b60233c1aeaaadeeb7c5ad009e6
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-22 17:58:19 +0300

    step-60

--- JS/Learn localStorage by Building a Todo App/main.js
+++ JS/Learn localStorage by Building a Todo App/main.js
@@ -109,19 +109,4 @@ taskForm.addEventListener("submit", (e) => {
   addOrUpdateTask();
 });
 
-const myTaskArr = [
-  { task: "Walk the Dog", date: "22-04-2022" },
-  { task: "Read some books", date: "02-11-2023" },
-  { task: "Watch football", date: "10-08-2021" },
-];
 
-localStorage.setItem("data", JSON.stringify(myTaskArr));
-console.log(localStorage);
-
-localStorage.clear();
-
-const getTaskArr = localStorage.getItem("data");
-console.log(getTaskArr);
-
-const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
-console.log(getTaskArrObj);

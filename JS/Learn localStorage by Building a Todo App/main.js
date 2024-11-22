84680a15128d4ad7865da8420c707818822b2881
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-22 17:14:17 +0300

    step-46

--- JS/Learn localStorage by Building a Todo App/main.js
+++ JS/Learn localStorage by Building a Todo App/main.js
@@ -52,7 +52,11 @@ const deleteTask = (buttonEl) => {
   taskData.splice(dataArrIndex, 1);
 };
 
-const editTask = (buttonEl) => {};
+const editTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+};
 
 const reset = () => {
   titleInput.value = "";

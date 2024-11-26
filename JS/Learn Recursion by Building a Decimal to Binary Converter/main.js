7ee91e13a0e00d6c535966b381f9fbff107083ce
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-26 13:15:43 +0300

    step-63

--- JS/Learn Recursion by Building a Decimal to Binary Converter/main.js
+++ JS/Learn Recursion by Building a Decimal to Binary Converter/main.js
@@ -1,6 +1,8 @@
 const countDownAndUp = (number) => {
   console.log(number);
   if (number === 0) {
+    console.log("Reached base case");
+    
     return;
   } else {
     countDownAndUp(number - 1);

639a9d874a7be4e1a4bbf2d6b0ea705b21940c9d
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-27 17:02:22 +0300

    step-93

--- JS/Learn Recursion by Building a Decimal to Binary Converter/main.js
+++ JS/Learn Recursion by Building a Decimal to Binary Converter/main.js
@@ -30,7 +30,9 @@ const showAnimation = () => {
   result.innerText = "Call Stack Animation";
   animationData.forEach((obj) => {
     setTimeout(() => {
-      animationContainer.innerHTML += ``;
+      animationContainer.innerHTML += `
+      <p id=""></p>
+      `;
     }, obj.addElDelay);
   });
 };

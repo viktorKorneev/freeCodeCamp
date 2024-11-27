2026190b4945c0050aa146125d5e55fdfa2c4c1a
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-27 16:45:25 +0300

    step-82

--- JS/Learn Recursion by Building a Decimal to Binary Converter/main.js
+++ JS/Learn Recursion by Building a Decimal to Binary Converter/main.js
@@ -11,7 +11,10 @@ const decimalToBinary = (input) => {
 };
 
 const showAnimation = () => {
-  console.log("free");
+  setTimeout(() => {
+    console.log("free");
+  }, 500);
+
   setTimeout(() => {
     console.log("Code");
   }, 1000);

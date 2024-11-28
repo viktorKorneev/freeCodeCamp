a9aec96e1570d1eda40fa1b2d872065216dee3e8
viktorKorneev <korneevviktor90@yandex.ru>
2024-11-28 20:12:51 +0300

    step-7

--- JS/Learn Regular Expressions by Building a Spam Filter/main.js
+++ JS/Learn Regular Expressions by Building a Spam Filter/main.js
@@ -1,7 +1,7 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
-const helpRegex = /please help/;
+const helpRegex = /please help/i;
 
 const isSpam = (msg) => false;
 

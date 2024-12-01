5a27c99070cc5958c89a6fc2a7c7ea9fada35e4a
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-01 14:05:14 +0300

    step-20

--- JS/Learn Basic Algorithmic Thinking by Building a Number Sorter/main.js
+++ JS/Learn Basic Algorithmic Thinking by Building a Number Sorter/main.js
@@ -4,7 +4,8 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
-  updateUI(inputValues);
+  const sortedValues = bubbleSort(inputValues);
+  updateUI(sortedValues);
 };
 
 const updateUI = (array = []) => {
@@ -17,8 +18,7 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-        console.log(array, array[j], array[j+1]);
-        
+      console.log(array, array[j], array[j + 1]);
     }
   }
 };

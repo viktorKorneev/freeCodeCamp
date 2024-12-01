c31331861271e2e5916bf75d7e095cd57d30aac1
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-01 14:39:14 +0300

    step-32

--- JS/Learn Basic Algorithmic Thinking by Building a Number Sorter/main.js
+++ JS/Learn Basic Algorithmic Thinking by Building a Number Sorter/main.js
@@ -32,7 +32,6 @@ const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     let minIndex = i;
     for (let j = i + 1; j < array.length; j++) {
-      console.log(array, array[j], array[minIndex]);
       if (array[j] < array[minIndex]) {
         minIndex = j;
       }

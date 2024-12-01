ff650f9ff8291453f6056b58df51e80d9a82cd19
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-01 13:50:04 +0300

    step-10

--- JS/Learn Basic Algorithmic Thinking by Building a Number Sorter/main.js
+++ JS/Learn Basic Algorithmic Thinking by Building a Number Sorter/main.js
@@ -4,6 +4,5 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
-  console.log(inputValues);
 };
 sortButton.addEventListener("click", sortInputArray);

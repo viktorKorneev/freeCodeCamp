c798e6491ba5acd85e4488999cf098a566a5eee3
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-02 16:03:54 +0300

    step-15

--- JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
+++ JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
@@ -4,8 +4,5 @@ const calculate = () => {
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
 };
 
-const getMean = (array) => {
-  const sum = array.reduce((acc, el) => acc + el, 0);
-  const mean = sum / array.length;
-  return mean;
-};
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;

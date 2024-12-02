61a90032e6aa3155736506d9789bc2a10f0c9436
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-02 16:11:41 +0300

    step-19

--- JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
+++ JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
@@ -1,6 +1,8 @@
 const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
 
+const getMedian = (array) => {};
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);

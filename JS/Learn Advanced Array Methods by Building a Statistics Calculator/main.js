35dbcca471b94d37be99eccfdb64d02560a8311a
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-03 13:47:55 +0300

    step-27

--- JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
+++ JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
@@ -3,6 +3,11 @@ const getMean = (array) =>
 
 const getMedian = (array) => {
   const sorted = array.toSorted((a, b) => a - b);
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
 };
 
 const calculate = () => {

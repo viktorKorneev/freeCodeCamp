253b22a47e6837efaca9b6c57ff2f4e18bb03013
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-03 14:27:05 +0300

    step-40

--- JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
+++ JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
@@ -16,7 +16,7 @@ const getMode = (array) => {
   if (new Set(Object.values(counts)).size === 1) {
     return null;
   }
-  const highest = Object.keys(counts);
+  const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
 };
 
 const calculate = () => {

2770efd0f44b6db9958c119548b23011ffe0ce67
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-03 14:22:51 +0300

    step-38

--- JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
+++ JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
@@ -12,8 +12,10 @@ const getMedian = (array) => {
 
 const getMode = (array) => {
   const counts = {};
-  array.forEach(el => counts[el] = counts[el] ? counts[el] + 1 : 1);
-  console.log(counts)
+  array.forEach(el => counts[el] = (counts[el] || 0) + 1);
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
 }
 
 const calculate = () => {

c25b77d573c25ee20f1713cfa8055f7e24b72185
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-04 15:57:16 +0300

    step-50

--- JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
+++ JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
@@ -30,11 +30,13 @@ const getRange = (array) => {
 
 const getVariance = (array) => {
   const mean = getMean(array);
-  const differences = array.map(
-    el => el - mean
-  );
+  const differences = array.map((el) => el - mean);
   const squaredDifferences = differences.map((el) => el ** 2);
-}
+  const sumSquaredDifferences = squaredDifferences.reduce(
+    (acc, el) => acc + el,
+    0
+  );
+};
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;

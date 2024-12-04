47dc9f7c556b7d9999bf0250d6acd6dce76075a1
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-04 16:07:41 +0300

    step-56

--- JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
+++ JS/Learn Advanced Array Methods by Building a Statistics Calculator/main.js
@@ -41,6 +41,7 @@ const getVariance = (array) => {
 
 const getStandardDeviation = (array) => {
   const variance = getVariance(array);
+  const standardDeviation = Math.pow(variance, 0.5);
 };
 
 const calculate = () => {

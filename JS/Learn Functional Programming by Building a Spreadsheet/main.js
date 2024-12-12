48e0e025a0ec3389f622645e08c86a22fe29f0bc
viktorKorneev <korneevviktor90@yandex.ru>
2024-12-12 18:46:31 +0300

    step-19

--- JS/Learn Functional Programming by Building a Spreadsheet/main.js
+++ JS/Learn Functional Programming by Building a Spreadsheet/main.js
@@ -25,6 +25,7 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+      container.appendChild(input);
     });
   });
 };
